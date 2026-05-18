fc63e6d7d395301f27d91803dcd81bfe094a4629
antsdz0r <aantoniclluka@gmail.com>
2026-05-18 18:54:35 +0200

    Fix za service/host

--- Service/Program.cs
+++ Service/Program.cs
@@ -7,11 +7,11 @@ namespace Service
     {
         static void Main(string[] args)
         {
-            EegService serviceInstance = new EegService();
+            
             try
             {
 
-                using (ServiceHost host = new ServiceHost(serviceInstance))
+                using (ServiceHost host = new ServiceHost(typeof(EegService)))
                 {
                     host.Open();
                     Console.WriteLine("[Service] EEG WCF Service started. Press ENTER to stop.");
@@ -22,11 +22,7 @@ namespace Service
             {
                 Console.WriteLine($"[Greska] {ex.Message}");
             }
-            finally
-            {
-                serviceInstance.Dispose();
-                Console.WriteLine("[Service] Resursi obrisani.");
-            }
+            
         }
     }
 }
